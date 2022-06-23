a81eb83a8c026ad973785f41bae2773e5152de17
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-23 20:02:48 -0300

    Adiciona exemplos  tipo de dados boolean

---
+++ variaveis-e-tipos/tiposDeDados-Boolean.js
@@ -0,0 +1,22 @@
+// videoaula ralacionada na DIO - tipos de dados Boolean
+https://web.dio.me/course/variaveis-e-tipos/learning/f808462a-8c0f-41be-acfb-2b128d830b70?back=/track/impulso-javascript-evolution&tab=undefined&moduleId=undefined
+
+
+
+
+// boolean
+
+let validation = 3 == 0;
+console.log(validation);
+validation = 3 > 4;
+console.log(validation);
+validation = 3 > 2;
+console.log(validation);
+validation.toString();
+console.log(validation);
+console.log(validation.toString());
+
+console.log(!validation);
+
+console.log(!false);
+console.log(!true);
