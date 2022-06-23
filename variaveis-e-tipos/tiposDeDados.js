f9f048db40b65b5dc324fd50738b343d0f0e0e40
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-23 17:17:27 -0300

    Adiciona exemplos tipo de dados string

---
+++ variaveis-e-tipos/tiposDeDados.js
@@ -0,0 +1,85 @@
+
+
+// // String
+// let um = "um";
+// console.log(um);
+// console.log(typeof(um));
+// //Tipagem Dinâmica
+// um = 1;
+// console.log(um);
+// console.log(typeof(um));
+
+
+
+
+// // interpolação
+// let firstName = "Ivan";
+// let lastName = "Matos";
+// let fullName = `Nome Completo: ${firstName} ${lastName}`;
+// console.log(fullName);
+
+
+
+
+
+// Tipos Primitivos
+// Não possuem métodos
+
+
+// // string tipo primitivo
+// let nome = "João";
+// let sobrenome = "Pedro";
+// let concatenado = nome.concat(sobrenome);
+// console.log(concatenado);
+// console.log(typeof(concatenado));
+// console.log(concatenado.length);
+// concatenado = nome + " " + sobrenome;
+// console.log(concatenado);
+// concatenado = nome + "\n" + sobrenome; //sequência de escape para pular linha \n
+// console.log(concatenado);
+// //concatenando de uma outra forma:
+// concatenado = `${nome} ${sobrenome}`;
+// console.log(concatenado);
+// //com a forma entre crases é mais simples formatar/pular linhas, etc
+// concatenado = `${nome}
+
+
+
+
+// let frase = "Olá, tudo bem?"
+// console.log(frase.split(""));
+// console.log(frase.split(" "));
+// console.log(frase);
+// console.log(frase.includes("tudo"));
+// console.log(frase.startsWith("O"));
+// console.log(frase.endsWith("?"));
+// console.log(frase.replace(",", "!"));
+// console.log(frase);
+
+
+
+
+//                           ${sobrenome}`;
+// console.log(concatenado);
+// //imprimir "" '', etc com sequência de escape
+// console.log("\"");
+// //imprimir "" '', etc entre crases {não precisa escape}
+// console.log('"');
+
+
+
+
+
+
+// //declarando string tipo primitivo vazia
+// let vazio = "";
+// console.log(typeof(vazio));
+
+
+
+// //String => (S)tring wrapper
+// let exemplo = new String("blablablá");
+// console.log(typeof(exemplo));
+// console.log(exemplo[0]); // b
+// console.log(exemplo[8]); // á
+// console.log(exemplo.length);
