fb4f29a505c148b0128f67e5b218d0e3081a87e1
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-23 15:19:03 -0300

    Adiciona prática sobre constantes

---
+++ variaveis-e-tipos/constantes.js
@@ -0,0 +1,22 @@
+//Constantes
+//Não se pode reatribuir o valor de uma constante
+//Não se pode redeclarar o valor de uma constante
+//Precisa ser inicializada antes de ser utilizada
+
+
+// //declaração padrão de constante
+// const FIRST_NAME =  "Ivan";
+// console.log(FIRST_NAME);
+
+
+
+// //Impossibilidade de hoisting de constante
+// FIRST_NAME = "Ivan";
+// console.log(FIRST_NAME);
+// const FIRST_NAME;
+// //retorna erro devido não poder fazer hosting de constante
+
+
+
+
+
