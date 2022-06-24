b2b41445f84b59085026e6d990c4a78cd2e4723e
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-23 21:54:12 -0300

    Adiciona exemplos de tipos de dados array

---
+++ variaveis-e-tipos/tiposDeDados-Arrays.js
@@ -0,0 +1,41 @@
+// // link aula relacionada na DIO - tipos de dados Arrays
+// https://web.dio.me/course/variaveis-e-tipos/learning/fcdd1211-5c7c-4745-bfbb-c04d97b1a028?back=/track/impulso-javascript-evolution&tab=undefined&moduleId=undefined
+
+
+// Arrays ou Vetores
+
+
+// let array = [];
+// console.log(array);
+
+
+
+// let array = new Array(); //outra forma de declarar o array, porque arrays são objetos
+// array.push("João", "Maria"); // push adiciona item(s) no final do array, ou no início no caso de um array vazio, obviamente
+// console.log(array);
+// array.push("Jomario");
+// console.log(array);
+// console.log(array.length);
+// array.pop(array); // retira o último elemento do array 
+// console.log(array);
+// array.shift(array); // retira o primeiro elemento do array
+// console.log(array);
+// array.unshift("elementoAdicionadoPeloUnshift"); // adiciona elemento no início do array
+// console.log(array);
+
+
+
+
+// // Iterabilidade
+// let array = ["vermelho", "azul", 2, 5, true];
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+// console.log(array.includes("azul"));
+// console.log("resultado20: " + (array.every(item => item === 5)));
+// console.log("resultado21: " + (array.some(item => item === 5)));
+
+// console.log(array);
+// console.log(array.reverse());
--- variaveis-e-tipos/tiposDeDados-Boolean.js
+++ variaveis-e-tipos/tiposDeDados-Boolean.js
@@ -1,5 +1,5 @@
-// videoaula ralacionada na DIO - tipos de dados Boolean
-https://web.dio.me/course/variaveis-e-tipos/learning/f808462a-8c0f-41be-acfb-2b128d830b70?back=/track/impulso-javascript-evolution&tab=undefined&moduleId=undefined
+// // videoaula ralacionada na DIO - tipos de dados Boolean
+// https://web.dio.me/course/variaveis-e-tipos/learning/f808462a-8c0f-41be-acfb-2b128d830b70?back=/track/impulso-javascript-evolution&tab=undefined&moduleId=undefined
 
 
 
